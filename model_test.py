d2d280672a2d3870d5f66560bf481ad571a0b08d
Barney Evans <evansba2701@gmail.com>
2025-09-02 16:36:01 +0100

    gemma3 4billion param model works for classifying emails

--- model_test.py
+++ model_test.py
@@ -1,9 +1,17 @@
 import requests
-from params import model_name, ollama_endpoint, prompt
+from params import model_name, ollama_endpoint, prompt, states
+
+#To prevent too many loops
+counter = 0
+
 
 data = {"model": model_name, "prompt": prompt, "stream": False}
 response = requests.post(ollama_endpoint, json=data)
 result = response.json()['response']
+while result not in states and counter < 7:
+    response = requests.post(ollama_endpoint, json=data)
+    result = response.json()['response']
+    counter = counter + 1
 
 with open("output.txt", "w") as f:
     f.write(result)
--- params.py
+++ params.py
@@ -1,23 +1,25 @@
 #The local ollama model to use:
-model_name = "gemma3:1b-it-qat"
+possible_models = ["gemma3:270m", "gemma3:270m-it-qat", "gemma3:1b", "gemma3:1b-it-qat", "gemma3:4b"]
+model_name = possible_models[4]
 
 #The endpoint for the local ollama server:
 ollama_endpoint = "http://localhost:11434/api/generate"
 
 #States for the prompt:
-states = ["Irrelevant", "Rejected", "Accepted", "Assessment Centre", "Interview", "Offer"]
+states = ["Irrelevant", "Not Sure", "Rejected", "Accepted", "Assessment Centre", "Online Test", "Interview", "Congrats you got the job!", "Job Application"]
 
 #The email we want to classify:
-email_content = "You have been invited to an interview for the Software Engineer position at TechCorp. Please let us know your availability."
+email_content = """Hi Barney, I want to sell you some cookies"""
 
-#The improved prompt:
+#Theprompt:
 prompt = f"""
 You are an expert email classifier. Your task is to classify the email content into one of the following categories. 
 The first category is for emails that are not relevant to job applications. 
+The second category is for emails that are job related but it's unclear what stage of the application process they pertain to.
 The other categories are for various stages of a job application process.
 
 Valid categories: {states}
-Your response must be a single word from the list and nothing else.
+Your response must be a single category from the list and nothing else.
 
 --- EMAIL CONTENT ---
 {email_content}
